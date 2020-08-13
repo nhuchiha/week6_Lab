acaacac5f3dc341ec79452d48f9d57d0374ece11
nhuchiha <nhuchiha2019@gmail.com>
2020-08-13 09:16:17 -0700

    Updated

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -3,7 +3,7 @@ public class Exercise2
 {
 	public static void main(String[] args) 
 	{
-		int N = 5; 
+		int N = 3; 
 		System.out.println(foo(N));
 	}
 	static int foo(int N) 
@@ -11,10 +11,10 @@ public class Exercise2
 		 int result = 0;
 		 for (int i=0; i<N; i++) //<--O(N) 
 			 result++;
-		 for (int j=0; j<1000000; j++) //<-- O(N)
+		 for (int j=0; j<1000000; j++) //<-- O(1) 
 			 result+=j;
 
-		 return result;
+		 return result; 
 	}
 }
-// The time complexity O(N)
+// O(N) + O(1) is still O(N). The time complexity is O(N)
--- src/Exercise3.java
+++ src/Exercise3.java
@@ -3,18 +3,15 @@ public class Exercise3
 {
 	public static void main(String[] args) 
 	{
-		int N = 10; 
+		int N = 6; 
 		System.out.println(bar(N));
 	}
 	static int bar(int N) 
 	{
 		 int result = 1;
-		 for (int i=1; i<N; i*=2) // <-- O(logN)
-		 {
+		 for (int i=1; i< N; i*=2) // <-- O(logN)
 			 result+=2;
-			 System.out.println(result);
-		 }
 		 return result;
 	}
 }
-//The time complexity is O(logN)
+//The loop variable i is multiplied by a constant number 2. Therefore, The time complexity is O(logN)
