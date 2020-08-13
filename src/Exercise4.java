f8720ea90a78ba80bf19ed3a09e383c681e7e023
nhuchiha <nhuchiha2019@gmail.com>
2020-08-12 20:55:38 -0700

    Updated Exercise 4

--- src/Exercise4.java
+++ src/Exercise4.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
-
+/* index       0  1   2   3   4   5   6   7   8   9
+ * The array: {1, 14, 15, 24, 55, 59, 73, 90, 94, 99}
+ * The number needs to search: 73
+ * First search: Middle element: list(middle) = list(4) = 55. Its left bound is 0 and its right bound is 9
+ * Since the middle number 55 < the searched number 73, we will continue the search with the later half of the array {59, 73, 90, 94, 99}
+ * Second search: Middle number: list(middle) = list(7) = 90. Its left bound is 5 and its right bound is 9. 
+ * Since the middle number 90 > the searched number 73, we will continue the search with the before half of the array {59,73}
+ * The third search: Middle number: list(middle) = list(5) = 59. Its left bound is 5 and its right bound is 6. 
+ * Since the middle number 59 < the searched number 73, we will continue the search with the later half of the array {73}
+ * The forth search: Middle number: list(middle) = list(7) = 73. If the middle number is equal to the search number, the number is found. 
+ */
 public class Exercise4 
 {
 
@@ -14,31 +24,33 @@ public class Exercise4
 		number = scan.nextInt();
 		scan.close();
 		
-		while (left <= right)
+		do 
 		{
 			int middle = (left + right) / 2; 
+		    
 			if(numList[middle] < number)
 			{
-				left = middle + 1;
-				System.out.println("The middle element: " + numList[middle]);
-				System.out.println("Left Bound: " + (middle-1));
-				System.out.println("Right Bound: " + (middle+1));
+				System.out.print("Left Bound: " + left + ". ");
+				System.out.print("Right Bound: " + right + ". ");
+				left = middle + 1;				
+				System.out.println("The middle element: " + numList[middle]);	
 			}
 			else if (numList[middle] > number)
 			{
+				System.out.print("Left Bound: " + left + ". ");
+				System.out.print("Right Bound: " + right + ". ");
 				right = middle - 1;
 				System.out.println("The middle element: " + numList[middle]);
-				System.out.println("Left Bound: " + (middle-1));
-				System.out.println("Right Bound: " + (middle+1));
 			}
 			else if(numList[middle] == number)
 			{
-					System.out.println("The number is the middle element: " + numList[middle]);
-					System.out.println("Left Bound: " + (middle-1));
-					System.out.println("Right Bound: " + (middle+1));
-					System.out.println("\n!!!Number is found!!!"); 
-					break;
+				System.out.print("Left Bound: " + left + ". ");
+				System.out.print("Right Bound: " + right + ". ");
+				System.out.println("The middle element: " + numList[middle]);
+				System.out.println("\n!!!Number is found!!!"); 
+				break;
 			}
 		}
+		while (left <= right);
 	}
 }
